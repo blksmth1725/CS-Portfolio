fde9121e184940b42a0958ec311f0179d700e434
Christian Sheen <csheen1725@gmail.com>
2021-04-23 12:26:10 -0400

    fixing commit reflection... yet again

--- src/components/pages/MyLearning.js
+++ src/components/pages/MyLearning.js
@@ -6,6 +6,9 @@ function MyLearning(props) {
       <div className="title-container">
         <h1 className="title">.myLearning()</h1>
       </div>
+      <div className="technical-skills"></div>
+      <div className="books"></div>
+      <div className="academia"></div>
     </div>
   );
 }
